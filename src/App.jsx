cd95497be9979b3eb0b97c236d13876c76a619b0
Anshul Rawat <rawatanshul522@gmail.com>
2022-07-21 13:53:24 +0530

    Added Show page, showing Show image, cast, Details, seasons etc

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 // eslint-disable-next-line react-hooks/exhaustive-deps
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import Navs from './components/Navs';
 import Home from './components/pages/Home';
 import Starred from './components/pages/Starred';
+import Show from './components/pages/Show';
+
 function App() {
   return (
     <Switch>
       <Route exact path="/" component={Home} />
       <Route exact path="/starred" component={Starred} />
+      <Route exact path="/show/:id" component={Show} />
       <Route>
         <div>404 Not Found</div>
       </Route>
--- src/misc/config.jsx
+++ src/misc/config.jsx
@@ -4,6 +4,7 @@ async function apiGet(queryString) {
   const response = await fetch(`${API_BASE_URL}${queryString}`).then(r =>
     r.json()
   );
+  //throw new Error('oops');
   return response;
 }
 export default apiGet;
