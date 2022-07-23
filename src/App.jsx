c84c5ef2189cd5b62761a9a4524b816d1ed0dccb
Anshul Rawat <rawatanshul522@gmail.com>
2022-07-23 10:49:49 +0530

    Added animation with react-fade-in

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,31 @@
 // eslint-disable-next-line react-hooks/exhaustive-deps
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+
 import Home from './components/pages/Home';
 import Starred from './components/pages/Starred';
 import Show from './components/pages/Show';
 
+const theme = {
+  mainColors: {
+    blue: '#2400ff',
+    gray: '#c6c6c6',
+    dark: '#353535',
+  },
+};
 function App() {
   return (
-    <Switch>
-      <Route exact path="/" component={Home} />
-      <Route exact path="/starred" component={Starred} />
-      <Route exact path="/show/:id" component={Show} />
-      <Route>
-        <div>404 Not Found</div>
-      </Route>
-    </Switch>
+    <ThemeProvider theme={theme}>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/starred" component={Starred} />
+        <Route exact path="/show/:id" component={Show} />
+        <Route>
+          <div>404 Not Found</div>
+        </Route>
+      </Switch>
+    </ThemeProvider>
   );
 }
 
--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,15 +1,24 @@
 import { React, useState } from 'react';
+
 import MainPageLayout from '../../components/MainPageLayout';
 import apiGet from '../../misc/config';
 import ShowGrid from '../show/ShowGrid';
 import ActorGrid from '../actor/ActorGrid';
+import { useLastQuery } from '../../misc/custom-hooks';
+import CustomRadio from '../CustomRadio';
+
+import {
+  SearchInput,
+  RadioInputsWrapper,
+  SearchButtonWrapper,
+} from './Home.styled';
 
 const Home = () => {
-  const [input, setInput] = useState('');
+  const [input, setInput] = useLastQuery();
   const [results, setResults] = useState(null);
   const [searchOption, setSearchOption] = useState('shows');
-  const isShowSearch = searchOption === 'shows';
 
+  const isShowSearch = searchOption === 'shows';
   const onInputChange = ev => {
     setInput(ev.target.value);
   };
@@ -43,39 +52,42 @@ const Home = () => {
   };
   return (
     <MainPageLayout>
-      <input
+      <SearchInput
         type="text"
         placeholder="Search for something"
         onChange={onInputChange}
         onKeyDown={onKeyDown}
         value={input}
       />
-      <div>
-        <label htmlFor="shows-search">
-          Shows
-          <input
+
+      <RadioInputsWrapper>
+        <div>
+          <CustomRadio
+            label="Shows"
             id="shows-search"
-            type="radio"
             value="shows"
             checked={isShowSearch}
             onChange={onRadioChange}
           />
-        </label>
+        </div>
 
-        <label htmlFor="actors-search">
-          Actors
-          <input
+        <div>
+          <CustomRadio
+            label="Actors"
             id="actors-search"
-            type="radio"
             value="people"
             checked={!isShowSearch}
             onChange={onRadioChange}
           />
-        </label>
-      </div>
-      <button type="button" onClick={onSearch}>
-        Search
-      </button>
+        </div>
+      </RadioInputsWrapper>
+
+      <SearchButtonWrapper>
+        <button type="button" onClick={onSearch}>
+          Search
+        </button>
+      </SearchButtonWrapper>
+
       {renderResults()}
     </MainPageLayout>
   );
